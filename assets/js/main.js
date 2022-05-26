fe827725bda555ec8516784631019868e3bfdc5c
Dali Hoffman (11005824) <HOFFMDE1@student.op.ac.nz>
2022-05-27 11:48:25 +1200

    Fixed the background images

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,15 @@ startImageTransition();
  
     function startImageTransition() {
  
-        let images = document.getElementsByClassName("test");
+        let images = document.getElementsByClassName("test");   // Selects the classes named test and assigns them to images
 
         
-        
-
- 
         // Set opacity of all images to 1
-        for (let i = 0; i < images.length; ++i) {
+        for (let i = 0; i < images.length; ++i) {          
             images[i].style.opacity = 1;
         }
  
-        let top = -100;
+        let top = -100;  // sets the 
  
  
         let cur = images.length - 1;
@@ -21,8 +18,7 @@ startImageTransition();
         setInterval(changeImage, 3000);
  
         async function changeImage() {
- 
-            
+           
             let nextImage = (1 + cur) % images.length;
 
             images[cur].style.zIndex = top + 1;
@@ -36,12 +32,28 @@ startImageTransition();
  
             top = top + 1;
  
-
             images[cur].style.opacity = 1;
 
             cur = nextImage;
+            
         }
  
+        let stops = document.querySelectorAll("overflow");
+
+        stops.forEach((e, i) => {
+            e.addEventListener("mouseenter", function (event) {         // Checks if hovering over the code removes all of the active classes and adds it to the one  which has a hover
+                stops.forEach(e =>{
+                    e.classList.remove("active")
+                });
+
+                e.classList.add("active");
+            });
+
+            e.addEventListener("mouseleave", function(event){        // Removes the mouse active class when the mouse isnt hovering over the text
+                e.classList.remove("active");
+            }, false);
+        });
+
 
         function transition() {
             return new Promise(function (resolve, reject) {
