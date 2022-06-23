478a20a5788166ba47dd083146ae3c9d8a2ee205
Dali Hoffman (11005824) <HOFFMDE1@student.op.ac.nz>
2022-06-23 14:01:36 +1200

    Delete(UnNeededJS): Delete unused Javascript throughout the website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+//Assortment of functions used to run through an array of images at an interval of 5 seconds changing the background image on the Homepage
+//This is done by changing the z-index of the image , then changing to it as needed
 startImageTransition();
 
     function startImageTransition() {
@@ -5,7 +7,7 @@ startImageTransition();
         const images = document.getElementsByClassName("test");   // Selects the classes named test and assigns them to images
         const stops = document.querySelectorAll("li .overflow");
 
-
+        //To be used to initialize the z-index of the slideshow
         let top = -100; 
         // Set opacity of all images to 1
         for (let i = 0; i < images.length; ++i) {          
@@ -17,13 +19,8 @@ startImageTransition();
  
         let cur = images.length - 1;
 
-        console.log(stops[0]);
-        console.log(stops[1]);
-        console.log(stops[2]);
-
- 
         setInterval(changeImage, 5000);     // Waits 5 seconds, then initiates the changeImage function
- 
+        //Function used to change the z-index on the upcoming image throughout the array
         async function changeImage() {
            
             top = top + 1;
@@ -47,26 +44,7 @@ startImageTransition();
             }
 
 
-            stops.forEach((e, i) => {
-                e.addEventListener("mouseenter", function (event) {         // Checks if hovering over the code removes all of the active classes and adds it to the one  which has a hover
-                    stops.forEach(e =>{
-                        e.classList.remove("active")                                 
-                    });
-                    if (e.classList.contains("active") || e.classList.contains("overflow")) {
-                        console.log("hello");
-                        images[cur] = images[0];
-                        
-                    }
-                    e.classList.add("active");               
-                });
-    
-                e.addEventListener("mouseleave", function(event){        // Removes the mouse active class when the mouse isnt hovering over the text
-                    e.classList.remove("active");
-                }, false);
-            });
-
-
-
+            //Function used to create a nice transition between images
         function transition() {
             return new Promise(function (resolve, reject) {
 
--- assets/js/second.js
+++ assets/js/second.js
@@ -35,18 +35,21 @@ fetch(`${BASE_URL}/search/photos?client_id=IW3Vq65vykO9B-bgkgHAJOXo_txwwK4qBs_sR
       button.innerHTML = ("+    ADD");
       teasimg.append(button);
       price = document.createElement("p");
+      price.classList.add("white");
       price.innerHTML = "$" +  Math.floor(Math.random() * 10) + 1; 
       teasimg.append(price);     
       wares.append(teasimg);
+      console.log(i);
     });
     // Fetching images from the unsplash website, Then adding an event listener that the background image changes on hover, 
     //Another event listener is also added so that the image changes back to the original once the mouse leaves the image 
-      fetch(`${BASE_URL}/search/photos?client_id=IW3Vq65vykO9B-bgkgHAJOXo_txwwK4qBs_sRjQPMR4&query=coffee`)              
+      fetch(`${BASE_URL}/search/photos?client_id=IW3Vq65vykO9B-bgkgHAJOXo_txwwK4qBs_sRjQPMR4&query=coffee/${FILTERCOLOR}`)              
       .then(response => response.json())                                                                                    
       .then(d => {                                                                                                          
         let children = wares.querySelectorAll(".tealight");
       d["results"].forEach((r, i) => {                                                                                      
           hovers[i] = `url(${r["urls"]["regular"]})`;
+          console.log(i);
       });
       children.forEach((e, i) => {
           e.addEventListener("mouseover", function (event) {                                                                 
